59cae40702d0a5180356f8ca0312f4395724673d
Avishek Verma <avishekverma79@gmail.com>
2022-08-22 19:08:11 +0530

    category setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-dark">
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="_">
               <strong style={{ color: "white" }}>React News</strong>
@@ -29,18 +29,44 @@ export class Navbar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    style={{ color: "white" }}
-                    aria-current="page"
-                    href="_"
-                  >
-                    Home
+                  <a className="nav-link" aria-current="page" href="_">
+                    All
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link" href="_">
+                    Business
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" style={{ color: "white" }} href="_">
-                    Link
+                  <a className="nav-link" href="_">
+                    Entertainment
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="_">
+                    General
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="_">
+                    Health
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="_">
+                    Science
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="_">
+                    Sports
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="_">
+                    Technology
                   </a>
                 </li>
               </ul>
--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 10,
+    category: "science",
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
   constructor() {
     super();
     this.state = {
@@ -13,7 +27,8 @@ export class News extends Component {
 
   // CDM runs after the render() function
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=79c438c4094749039a2e537d1d603109&page=${this.state.page}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=79c438c4094749039a2e537d1d603109&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
@@ -21,15 +36,23 @@ export class News extends Component {
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
   nextPage = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=79c438c4094749039a2e537d1d603109&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=79c438c4094749039a2e537d1d603109&page=${
       this.state.page + 1
-    }&pageSize=20`;
-
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
     } else {
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -37,38 +60,51 @@ export class News extends Component {
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
 
   prevPage = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=79c438c4094749039a2e537d1d603109&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=79c438c4094749039a2e537d1d603109&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
+
+    this.setState({ loading: true });
+
     let data = await fetch(url);
     let parsedData = await data.json();
 
-    this.setState({ page: this.state.page - 1, articles: parsedData.articles });
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false,
+    });
   };
 
   render() {
     return (
       <div className="container my-3">
         <h2>Top Headlines</h2>
-
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imgUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4 my-2" key={element.url}>
+                  <NewsItem
+                    title={element.title}
+                    description={element.description}
+                    imgUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="d-flex justify-content-between my-5">
           <button
@@ -78,9 +114,14 @@ export class News extends Component {
           >
             &larr; Previous
           </button>
+
+          <h3 style={{ fontSize: "17px", color: "grey" }}>
+            Page {this.state.page}
+          </h3>
           <button
             disabled={
-              this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
             }
             className="btn btn-dark"
             onClick={this.nextPage}
