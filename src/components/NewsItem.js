9fdc444c6993efa72dfab74641a08902c2abb603
Avishek Verma <avishekverma79@gmail.com>
2022-08-22 10:29:17 +0530

    fetched news using api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>NewsItem Component</div>;
+    let { title, description, imgUrl, newsUrl } = this.props;
+    return (
+      <div>
+        <div className="card">
+          <img
+            src={
+              imgUrl ??
+              "https://harshatimbers.com/wp-content/themes/harshatimbers/images/no-img.jpgnpm run start"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} target=" " className="btn btn-sm btn-primary">
+              Expand
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
